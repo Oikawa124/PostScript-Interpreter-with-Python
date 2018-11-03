84fc6276f60523df3a7eeb9a487edbde0b313a06
devel <oiku1515@gmail.com>
2018-11-04 08:39:35 +0900

    [update] 入力する文字列をassertを追加

--- 01_int_parser/int_parser.py
+++ 01_int_parser/int_parser.py
@@ -2,9 +2,20 @@
 # -*- coding: utf-8 -*-
 # Created by devel on 2018/11/04.
 
+input = "123 456 1203"
+
+
+
 
 def main():
+    answer1 = 0
+    answer2 = 0
+    answer3 = 0
 
+
+    assert (answer1 == 123)
+    assert (answer1 == 145)
+    assert (answer1 == 1203)
 
 if __name__ == '__main__':
     main()
