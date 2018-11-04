95899aa60750ec0297527a25e5608df461d596fa
devel <oiku1515@gmail.com>
2018-11-04 11:16:27 +0900

    [add] parser.pyに、02からコピペ

---
+++ 03_parser/getc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by devel on 2018/11/04.
+
+input = "123 456"
+pos = 0
+
+def getc ():
+    global pos
+    if len(input) == pos:
+        return '\0'
+    ch = input[pos]
+    pos += 1
+    return ch
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- 03_parser/parser.py
+++ 03_parser/parser.py
@@ -3,7 +3,11 @@
 # Created by devel on 2018/11/04.
 from enum import IntEnum, auto
 
-class LexicalType(IntEnum):
+from getc import getc
+
+
+class Ltype(IntEnum):
+    EOF = -1
     NUMBER = auto()
     SPACE = auto()
     EXECUTABLE_NAME = auto()
@@ -13,8 +17,43 @@ class LexicalType(IntEnum):
     END_OF_FILE = auto()
     UNKNOWN = auto()
 
+
+class Token:
+    def __init__(self):
+        self.Ltype = None
+        self.number = 0
+        self.name = ""
+
+
+def parse_one(prev_ch):
+    token = Token()
+    ch = getc() if prev_ch == Ltype.EOF else prev_ch
+
+    if ch.isalnum():
+        num = 0
+        while ch.isalnum():
+            num = num*10 + int(ch)
+            ch = getc()
+        token.Ltype = Ltype.NUMBER
+        token.number = num
+        return ch, token
+    elif ch.isspace():
+        while ch.isspace(): ch = getc()
+
+        token.Ltype = Ltype.SPACE
+        token.name = " "
+        return ch, token
+
+
+
 def main():
-    pass
+    ch1, token1 = parse_one(Ltype.EOF)
+    ch2, token2 = parse_one(ch1)
+    ch3, token3 = parse_one(ch2)
+
+    assert (token1.number == 123)
+    assert (token2.name == " ")
+    assert (token3.number == 456)
 
 if __name__ == '__main__':
     main()
