59f9ed7b30f94dfca720a77333cfd25d41852641
devel <oiku1515@gmail.com>
2018-11-04 10:13:29 +0900

    first commit

---
+++ 02_int_parser_getc/get_token_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by devel on 2018/11/04.
+
+input = "123 456"
+pos = 0
+
+def get_token_list ():
+    return input.split()
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
