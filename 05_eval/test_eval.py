535002c32ed98255d3ffe955c3ceef9f494c8cdb
devel <oiku1515@gmail.com>
2018-11-05 17:00:24 +0900

    [update] evalのaddに関するtestを一つ追加した

--- 05_eval/test_eval.py
+++ 05_eval/test_eval.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by devel on 2018/11/05.
+from my_parser import gets_set_src
+from stack import Stack, Etype, Element
+from eval import eval
 
 
-def main():
-    pass
+def test_eval_add():
+    expect = Element(etype=Etype.NUMBER, value=3)
 
-if __name__ == '__main__':
-    main()
+    _input = "1 1 1 add add"
+    gets_set_src(_input)
+    stack = Stack()
+    eval(stack)
+    actual = stack.pop()
+
+    assert expect.value == actual.value
