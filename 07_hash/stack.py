e9e651778ab85e43ff13cf1ec146bb90e492e176
devel <oiku1515@gmail.com>
2018-11-08 18:21:13 +0900

    first commit

---
+++ 07_hash/stack.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by devel on 2018/11/05.
+from enum import IntEnum, auto
+from collections import namedtuple
+
+class Etype(IntEnum):
+    NUMBER = auto()
+    EXECUTABLE_NAME = auto()
+    LITERAL_NAME = auto()
+    NOT_EXIST = auto()
+
+
+Element = namedtuple("Element", ("etype", "value"))
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, elem):
+        self.stack.append(elem)
+
+    def pop(self):
+        try:
+            return self.stack.pop()
+        except IndexError:
+            raise
+
+    def print_all(self):
+        for i, v in enumerate(self.stack):
+            print(f"{i}:{v}")
