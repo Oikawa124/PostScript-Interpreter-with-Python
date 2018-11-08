5a5cca3cf169ebb7d00f07dcee083228c6784611
devel <oiku1515@gmail.com>
2018-11-08 16:30:34 +0900

    first commit

---
+++ 06_literal_name/element.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by devel on 2018/11/08.
+from my_parser import Ltype, parse_one
+from stack import *
+from eval import gets
+
+def next_token(gene):
+    try:
+        next(gene)
+    except StopIteration:
+        raise
+
+def to_elems(gene):
+    token, words = parse_one(gene)
+
+    while token.ltype != Ltype.END_OF_FILE:
+        if token.ltype == Ltype.NUMBER:
+            yield Element(etype=Etype.NUMBER, value=token.value)
+        elif token.ltype == Ltype.EXECUTABLE_NAME:
+            yield Element(etype=Etype.EXECUTABLE_NAME, value=token.value)
+        elif token.ltype == Ltype.LITERAL_NAME:
+            yield Element(etype=Etype.LITERAL_NAME, value=token.value)
+
+        token, words = parse_one(words)
+
+def main():
+    elems = to_elems(gets("abc　def g h 11"))
+
+    for i in elems:
+        print(i)
+
+if __name__ == '__main__':
+    main()
---
+++ 06_literal_name/eval.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by devel on 2018/11/05.
+from element import *
+
+def gets(input_):
+    return (x for x in input_)
+
+
+def eval(elems, stack):
+
+    def add_op():
+        num1 = stack.pop()
+        num2 = stack.pop()
+        _sum = num1.value + num2.value
+        stack.push(Element(etype=Etype.NUMBER, value=_sum))
+
+    for elem in elems:
+        if elem.etype == Etype.NUMBER:
+            stack.push(elem)
+        elif elem.etype == Etype.EXECUTABLE_NAME:
+            if elem.value == "add":
+                add_op()
+            else:
+                stack.push(elem)
+        elif elem.etype == Etype.LITERAL_NAME:
+            stack.push(elem)
+        else:
+            print("Not come here")
+
+def main():
+    stack = Stack()
+    eval(to_elems(gets("1 1 add 1 3 /a")), stack)
+
+    stack.print_all()
+
+
+if __name__ == '__main__':
+    main()
