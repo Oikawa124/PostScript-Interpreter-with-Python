d5b01ed5c72e8a2707852a6b0691918925f8ffc4
devel <oiku1515@gmail.com>
2018-11-11 05:39:40 +0900

    [update] evalの変更にともない、テストの実装を変更

--- 08_register_primitives/test_eval.py
+++ 08_register_primitives/test_eval.py
@@ -6,31 +6,29 @@ from eval import *
 def test_eval_add():
     expect = Element(etype=Etype.NUMBER, value=3)
 
-    stack = Stack()
-    eval(to_elems(to_char_gen("1 1 1 add add")), stack)
-    actual = stack.pop()
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("1 1 1 add add")))
+    actual = evaluator.stack.pop()
 
     assert expect.value == actual.value
 
 def test_eval_dict():
     expect = Element(etype=Etype.NUMBER, value=1)
 
-    stack = Stack()
-    mydict = MyDict()
-    eval(to_elems(to_char_gen("/a 1 def")), stack, mydict)
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("/a 1 def")))
 
     key = Element(Etype.LITERAL_NAME, "a")
-    actual = mydict.get(key)
+    actual = evaluator.dict.get(key)
 
     assert expect.value == actual.value
 
 def test_eval_dict_stack_pop():
     expect = Element(etype=Etype.NUMBER, value=1)
 
-    stack = Stack()
-    mydict = MyDict()
-    eval(to_elems(to_char_gen("/a 1 def a")), stack, mydict)
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("/a 1 def")))
 
-    actual = stack.pop()
+    actual = evaluator.stack.pop()
 
     assert expect.value == actual.value
