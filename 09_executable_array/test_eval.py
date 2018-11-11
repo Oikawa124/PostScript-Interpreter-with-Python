63eebbe5ddba5aa7008407cab54431f5cefd7305
devel <oiku1515@gmail.com>
2018-11-12 02:34:19 +0900

    [update] 実行可能配列に関するテストを追加

--- 09_executable_array/test_eval.py
+++ 09_executable_array/test_eval.py
@@ -65,3 +65,46 @@ def test_eval_div():
     actual = evaluator.stack.pop()
 
     assert expect.value == actual.value
+
+def test_eval_exec_arr_number():
+    expect = Element(etype=Etype.NUMBER, value=3)
+
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("/a {3} def a")))
+    actual = evaluator.stack.pop()
+
+    assert expect.value == actual.value
+
+def test_eval_exec_arr_two_numbers():
+    expect1 = Element(etype=Etype.NUMBER, value=3)
+    expect2 = Element(etype=Etype.NUMBER, value=5)
+
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("/a {3 5} def a")))
+
+    actual2 = evaluator.stack.pop()
+    actual1 = evaluator.stack.pop()
+
+    assert expect1.value == actual1.value
+    assert expect2.value == actual2.value
+
+
+def test_eval_exec_arr_literal_name():
+    expect = Element(etype=Etype.LITERAL_NAME, value="abc")
+
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("/a {/abc} def a")))
+    actual = evaluator.stack.pop()
+
+    assert expect.value == actual.value
+
+
+def test_eval_exec_arr_executable_name():
+    expect = Element(etype=Etype.EXECUTABLE_NAME, value="abc")
+
+    evaluator = Evaluator()
+    evaluator.eval(to_elems(to_char_gen("/a {abc} def a")))
+    actual = evaluator.stack.pop()
+
+    assert expect.value == actual.value
+
